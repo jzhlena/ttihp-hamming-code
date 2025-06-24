dd9cf0e9f9ec191a6faa9adbaab85d65100c383e
Cynthia Ma <c29ma@uwaterloo.ca>
2025-06-24 12:55:13 -0400

    suggested change to decoder

--- src/hamming_decode.v
+++ src/hamming_decode.v
@@ -6,14 +6,10 @@
 `default_nettype none
 
 module hamming_decoder (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output reg [7:0] uo_out,   // Dedicated outputs
-    // input  wire [7:0] uio_in,   // IOs: Input path
-    // output wire [7:0] uio_out,  // IOs: Output path
-    // output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    // input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in, //code_in,    // Dedicated inputs
+    output reg [7:0] uo_out, //code_out,   // Dedicated outputs
+    output reg [3:0] syndrome,  // Syndrome bits: 3 bits for error location, 0 for no error or uncorrectable error
+    output reg [1:0] error_flag   // Error flag: 00 = no error, 01 = single-bit error, 10 = double-bit error
 );
 
     // declare temporary signals for calculated check bits and syndrome bits
